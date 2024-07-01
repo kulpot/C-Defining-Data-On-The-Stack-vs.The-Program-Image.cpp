704b9035f5fb94350148d6247a221cc1493145a0
Kulpot <kulpotwahu@gmail.com>
2024-07-01 12:30:12 +0800

    https://www.youtube.com/watch?v=mDx7YspeCqw&list=PLRwVmtr-pp04StYuWs0n765aXyrlL2kl1&index=2

--- C++ Defining Data On The Stack vs.The Program Image/CppScratchPad.cpp
+++ C++ Defining Data On The Stack vs.The Program Image/CppScratchPad.cpp
@@ -2,6 +2,8 @@
 using std::cout;
 using std::endl;
 
+//C++ - Defining Data On The Stack vs. The Program Image   //ref link:https://www.youtube.com/watch?v=mDx7YspeCqw&list=PLRwVmtr-pp04StYuWs0n765aXyrlL2kl1&index=2
+
 void main()
 {
 
