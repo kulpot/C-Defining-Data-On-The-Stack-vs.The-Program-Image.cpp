6b1e603e68ddf750ccd35306d3fbf63df5ee3a5c
Kulpot <kulpotwahu@gmail.com>
2024-07-02 12:37:30 +0800

    ..

--- C++ Defining Data On The Stack vs.The Program Image/CppScratchPad.cpp
+++ C++ Defining Data On The Stack vs.The Program Image/CppScratchPad.cpp
@@ -4,7 +4,7 @@ using std::endl;
 
 //C++ - Defining Data On The Stack vs. The Program Image   //ref link:https://www.youtube.com/watch?v=mDx7YspeCqw&list=PLRwVmtr-pp04StYuWs0n765aXyrlL2kl1&index=2
 
-int myInt;
+int myInt; //this is same as static int, global to anything,
 
 void main()
 {
